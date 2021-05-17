540b1f39947d81d21ec8988d9c306197043a135f
dapp.programmer@gmail.com <dapp.programmer@gmail.com>
2021-05-17 22:10:38 +0530

    written tests for token contract to test initial deployment

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.1;
 
 contract Token {
-    string public name = 'My Name';
+    string public name = 'Leo Token';
+    string public symbol = 'LEO';
+    uint public decimals = 18;
+    uint public totalSupply = 10;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
---
+++ test/Token.test.js
@@ -0,0 +1,37 @@
+const Token = artifacts.require('./Token');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+contract('Token', (accounts) => {
+    let token;
+    const name = 'Leo Token';
+    const symbol = 'LEO';
+    const decimals = '18';
+    const totalSupply = '1000000000000000000000000';
+
+
+    beforeEach(async()=>{
+        token = await Token.new();
+    })
+
+    describe('deployment', () => {
+        it('tracks the name', async() => {
+            const result = await token.name();
+            result.should.equal(name);
+        })
+        it('tracks the symbol', async() => {
+            const result = await token.symbol();
+            result.should.equal(symbol);
+        })
+        it('tracks the decimals', async() => {
+            const result = await token.decimals();
+            result.toString().should.equal(decimals.toString());
+        })
+        it('tracks the total supply', async() => {
+            const result = await token.totalSupply();
+            result.toString().should.equal(totalSupply);
+        })
+    })
+})
